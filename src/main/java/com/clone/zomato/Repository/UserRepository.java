60a61f86c7b063ee4c541779e0b3fcf074ed8598
Ankitraj21 <rajankit2121@gmail.com>
2025-01-20 08:34:25 +0530

    Workflow Test

--- src/main/java/com/clone/zomato/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.clone.zomato.Repository;
-
-import com.clone.zomato.Entity.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserInfo,String> {
-}
